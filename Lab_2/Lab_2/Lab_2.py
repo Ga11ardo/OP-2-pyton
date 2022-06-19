516c9a24a935e1871a2147168bacc5ccd9766504
Ga11ardo <90717318+Ga11ardo@users.noreply.github.com>
2022-06-19 23:54:28 +0300

    Add files via upload

---
+++ Lab_2/Lab_2/Lab_2.py
@@ -0,0 +1,7 @@
+import funcs
+
+if __name__=="__main__":
+    path = "PhoneBook.dat"
+    funcs.outputFile(path) # виведення вмісту файлу
+    funcs.countSum(path) # обрахування тарифу
+    funcs.outputFile(path)
